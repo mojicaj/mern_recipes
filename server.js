89310e26573ef8ee3cdf4ff25ad2f73be7583471
Jason Mojica <mojicaj@gmail.com>
2018-09-14 20:16:43 -0400

    Handle CORS

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(bodyParser.json())
 
 app.use(express.static('/app/public'))
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    next();
+});
+
 app.use(routes)
 
 const PORT = process.env.PORT || 3000
