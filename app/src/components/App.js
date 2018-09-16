1e54214fe817a6e0914b47da0004b55399b1d0c0
Jason Mojica <mojicaj@gmail.com>
2018-09-15 20:53:09 -0400

    Add new recipe button

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import './styles/App.css';
-import Recipes from './Recipes';
+import React, { Component } from "react";
+import "./styles/App.css";
+import Recipes from "./Recipes";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header px-3">
           <h1>Good Eats</h1>
+          <button>New Recipe</button>
         </header>
-
         <Recipes />
       </div>
     );
