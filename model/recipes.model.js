b5d7afa7a1a404c709e3f7d23d5e5703ff251f45
Jason Mojica <mojicaj@gmail.com>
2018-09-18 22:48:54 -0400

    Fix model

--- model/recipes.model.js
+++ model/recipes.model.js
@@ -1,50 +1,45 @@
-let mongoose = require('mongoose')
+let mongoose = require("mongoose");
 
-const server = process.env.DB_SERVER
-const database = process.env.DB
-const user = process.env.DB_USER
-const password = process.env.DB_PASSWORD
+const server = process.env.DB_SERVER;
+const database = process.env.DB;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
-mongoose.connect(`mongodb://${user}:${password}@${server}/${database}`)
+mongoose.connect(`mongodb://${user}:${password}@${server}/${database}`);
 
 let Ingredient = new mongoose.Schema({
-    measurement: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    }
-
-})
+  measurement: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+});
 
 let RecipeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    servings: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    prep: String,
-    cook: String, 
-    ingredients: 
-    {
-        type: [Ingredient],
-        required: true,
-        unique: true
-    },
-    instructions: {
-        type: String,
-        required: true
-    },
-    image: String
-})
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  servings: {
+    type: String,
+    required: true
+  },
+  prep: String,
+  cook: String,
+  ingredients: {
+    type: [Ingredient],
+    required: true,
+    unique: true
+  },
+  instructions: {
+    type: String,
+    required: true
+  },
+  image: String
+});
 
-module.exports = mongoose.model('Recipe', RecipeSchema)
+module.exports = mongoose.model("Recipe", RecipeSchema);
