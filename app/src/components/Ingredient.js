2848f7e9ec35c03b019b42f8ce2b5036d2b1786c
Jason Mojica <mojicaj@gmail.com>
2018-09-18 22:47:08 -0400

    Refactor to functional component

--- app/src/components/Ingredient.js
+++ app/src/components/Ingredient.js
@@ -1,35 +1,11 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Ingredient extends Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.ingredient.name} - {this.props.ingredient.measurement}</p>
-            </div>
-        )
-    }
-}
+const Ingredient = ({ ingredient }) => (
+  <div>
+    <p>
+      {ingredient.name} - {ingredient.measurement}
+    </p>
+  </div>
+);
 
 export default Ingredient;
-
-/*
-Recipe JSON:
-{
-    "name": "Buffalo Wings",
-    "servings": "6",
-    "prep": "30 minutes",
-    "cook": "2 hours", 
-    "ingredients": [
-        {
-            "measurement": "1 lb",
-            "name": "Chicken wings"
-        },
-        {
-            "measurement": "3 cups",
-            "name": "Hot sauce"
-        }
-    ],
-    "instructions": "bake wings in oven at 300 degrees. Stir the pot, cover the wings",
-    "image": ""
-}
-*/
