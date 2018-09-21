812feb31614617c24f55b640503c1b35814076c2
Jason Mojica <mojicaj@gmail.com>
2018-09-20 21:24:51 -0400

    Improve full recipe display

--- app/src/components/Recipe.js
+++ app/src/components/Recipe.js
@@ -20,24 +20,61 @@ const RecipeCard = ({ recipe }) => (
 
 // Full display for individual recipe page
 const RecipeFull = ({ recipe }) => (
-  <div>
-    <img src={recipe.image} alt={recipe.name} />
-    <h3>{recipe.name}</h3>
-    <p>Servings: {recipe.servings}</p>
-    <p>Prep time: {recipe.prep}</p>
-    <p>Cook time: {recipe.cook}</p>
-    <h5>Ingredients:</h5>
-    <ul>
-      {recipe.ingredients.map(ingredient => {
-        return (
-          <li key={ingredient["_id"]}>
-            <Ingredient ingredient={ingredient} />
-          </li>
-        );
-      })}
-    </ul>
-    <h5>Directions:</h5>
-    <p>{recipe.instructions}</p>
+  <div className="container w-75 recipe">
+    <div className="row">
+      <div className="col">
+        <img
+          src={recipe.image}
+          className="img-fluid img-thumbnail"
+          alt={recipe.name}
+        />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <h3>{recipe.name}</h3>
+      </div>
+    </div>
+    <div className="row info rounded-top">
+      <div className="col">
+        <h6>Servings:</h6>
+        <p>{recipe.servings}</p>
+      </div>
+      <div className="col">
+        <h6>Prep time:</h6>
+        <p>{recipe.prep}</p>
+      </div>
+      <div className="col">
+        <h6>Cook time:</h6>
+        <p>{recipe.cook}</p>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <h5>Ingredients:</h5>
+      </div>
+      <div className="w-100" />
+      <div className="col">
+        <ul>
+          {recipe.ingredients.map(ingredient => {
+            return (
+              <li key={ingredient["_id"]}>
+                <Ingredient ingredient={ingredient} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <h5>Directions:</h5>
+      </div>
+      <div className="w-100" />
+      <div className="col">
+        <p>{recipe.instructions}</p>
+      </div>
+    </div>
   </div>
 );
 
